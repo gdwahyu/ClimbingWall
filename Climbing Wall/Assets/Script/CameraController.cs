d4773586231da91a97bfa65aed5b65cf82e5b44f
Gd Wahyu Nugraha <gdwahyu@users.noreply.github.com>
2019-04-25 21:28:34 +0700

    update 25 april

---
+++ Climbing Wall/Assets/Script/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        if (Input.GetKey ("w"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
